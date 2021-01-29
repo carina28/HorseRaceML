6bb402043eb79e4e772f54aacfb15979c4688bf1
mkurmarc <51379562+mkurmarc@users.noreply.github.com>
2021-01-29 10:23:49 -0800

    edited login.html

--- horserace/models.py
+++ horserace/models.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from horserace import db
 
 class User(db.Model):
-    __tablename__ = 'user'
+    __tablename__ = 'users'
     user_id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
