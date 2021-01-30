31302fd1999a36634d8d61a57cfb3a6be0dc06f3
mkurmarc <51379562+mkurmarc@users.noreply.github.com>
2021-01-30 08:22:45 -0800

    done, turn in now

--- horserace/__init__.py
+++ horserace/__init__.py
@@ -10,7 +10,6 @@ app = Flask(__name__) # initializes
 app.static_folder = 'static'
 app.debug = True
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Zergling22!@localhost/triple-crown'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://yipfhhdxmzkbkh:393c7f6e2f463b863fd761bf12b66107e97da542a72c1a839fd5f41b0da2d9be@ec2-3-220-23-212.compute-1.amazonaws.com:5432/de9v3q9972n38b'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
